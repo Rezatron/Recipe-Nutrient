37867f9400b5ff942a8b1b41b4ae03a7c8d549d4
Adam Reza <adam-reza@hotmail.co.uk>
2024-02-26 18:01:58 +0000

    pop up implemented. next: change layout to look like UK 'nutritional facts'

--- static/script.js
+++ static/script.js
@@ -1,11 +1,16 @@
-// Update the JavaScript to toggle visibility when the button is clicked
+// Updated JavaScript to toggle details popup
+// Get all "See more details" buttons
 var detailButtons = document.querySelectorAll('.see-details-button');
 
+// Add click event listener to each button
 detailButtons.forEach(function (button) {
     button.addEventListener('click', function () {
-        var detailsContainer = button.parentElement.querySelector('.details-popup');
-        if (detailsContainer) {
-            detailsContainer.classList.toggle('active');
+        // Toggle the visibility of details popup
+        var detailsPopup = button.closest('.recipe-item').querySelector('.details-popup'); // Get the details popup within the recipe item
+        if (detailsPopup) {
+            detailsPopup.classList.toggle('active');
+        } else {
+            console.error("Details popup not found.");
         }
     });
 });
